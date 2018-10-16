45e4482b008e92089d59a5696699f1a956200db1
alberto.martinez <alberto.martinez@edorasware.com>
2018-10-16 19:39:19 +0200

    Add build script

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,68 +2,75 @@ const path = require("path"),
   webpack = require("webpack"),
   HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    app: ["./src/app.tsx"],
-    vendor: ["react", "react-dom"]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].bundle.js"
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ["babel-loader", "source-map-loader"],
-        exclude: [/node_modules/]
-      },
-      {
-        test: /\.(t|j)sx?$/,
-        use: [
-          {
-            loader: "babel-loader"
-          },
-          {
-            loader: "awesome-typescript-loader",
-            options: {
-              transpileOnly: true,
-              logLevel: "debug",
-              silent: true
+module.exports = (env = {}) => {
+  let config = {
+    entry: {
+      app: ["./src/app.tsx"],
+      vendor: ["react", "react-dom"]
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "js/[name].bundle.js"
+    },
+    devtool: "source-map",
+    resolve: {
+      extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: ["babel-loader", "source-map-loader"],
+          exclude: [/node_modules/]
+        },
+        {
+          test: /\.(t|j)sx?$/,
+          use: [
+            {
+              loader: "babel-loader"
+            },
+            {
+              loader: "awesome-typescript-loader",
+              options: {
+                transpileOnly: true,
+                logLevel: "debug",
+                silent: true
+              }
             }
-          }
-        ],
-        exclude: [/node_modules/, /\.(test|spec|e2e)\.ts$/]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-      {
-        test: /\.(png|gif|jpe?g|svg|ico)$/,
-        include: path.join(__dirname, "src", "assets"),
-        use: [
-          {
-            loader: "file-loader"
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, "index.html") })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000
+          ],
+          exclude: [/node_modules/, /\.(test|spec|e2e)\.ts$/]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          ]
+        },
+        {
+          test: /\.(png|gif|jpe?g|svg|ico)$/,
+          include: path.join(__dirname, "src", "assets"),
+          use: [
+            {
+              loader: "file-loader"
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, "index.html") })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000
+    }
+  };
+
+  if (env.production) {
+    delete config.devtool;
   }
+  return config;
 };
