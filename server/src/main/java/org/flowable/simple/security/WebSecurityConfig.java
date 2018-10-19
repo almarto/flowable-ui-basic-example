b76f22011c285a6200bacd4119f39c59ea0e5f75
inaki.huete <inaki.huete@flowable.com>
2018-10-19 18:25:31 +0200

    Disable security and enable cors

--- server/src/main/java/org/flowable/simple/security/WebSecurityConfig.java
+++ server/src/main/java/org/flowable/simple/security/WebSecurityConfig.java
@@ -1,36 +1,36 @@
-package org.flowable.simple.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * @author Inaki Huete
- */
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-        throws Exception {
-        auth
-            .inMemoryAuthentication()
-            .withUser("rest-admin")
-            .password("test")
-            .roles("USER", "ADMIN");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
-    }
-
-}
+//package org.flowable.simple.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author Inaki Huete
+// */
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//        throws Exception {
+//        auth
+//            .inMemoryAuthentication()
+//            .withUser("rest-admin")
+//            .password("test")
+//            .roles("USER", "ADMIN");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()
+//            .anyRequest()
+//            .authenticated()
+//            .and()
+//            .httpBasic();
+//    }
+//
+//}
